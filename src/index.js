5ec7acbc2b3d531a342f8357504b00fd8fee31f5
maxblessed91 <vadams3@yandex.ru>
2020-10-26 00:19:47 +0400

    completed task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = [],
+        sign = "";
+
+    for (i = 0; (i + 10) <= expr.length; i += 10) {
+		sign = expr.substring(i, i + 10);
+
+        if (sign == "**********") letters.push(" ");
+        else letters.push(Number(sign).toString());
+    }
+
+    for (i = 0; i < letters.length; i++) {
+        letters[i] = letters[i].replace(/10/g, '.').replace(/11/g, '-').replace(/([.-]+[-.]*)/g, (_, x) => MORSE_TABLE[x]);
+    }
+
+    return letters.join("");
 }
 
 module.exports = {
